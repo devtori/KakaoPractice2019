0ae5f8c267cbacda87cc3ade097e957545b57721
devtori <chohj816@gmail.com>
2018-09-15 00:02:53 +0900

    init

--- src/practice0_1.java
+++ src/practice0_1.java
@@ -1,4 +1,9 @@
-import java.util.Scanner;
+
+
+/**
+ * 길이가 n인 배열에 1부터 n까지 숫자가 중복 없이 한 번씩 들어 있는지를 확인하려고 합니다.
+ * 1부터 n까지 숫자가 중복 없이 한 번씩 들어 있는 경우 true를, 아닌 경우 false를 반환하도록 함수 solution을 완성해주세요.
+ */
 
 public class practice0_1 {
 
